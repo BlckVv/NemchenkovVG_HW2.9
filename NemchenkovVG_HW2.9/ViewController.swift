e11364e4d4fe803b04c040e422c56b40d2edee1a
NemchenkovVG <blckvv@yandex.ru>
2021-08-04 18:20:41 +0400

    Initial Commit

---
+++ NemchenkovVG_HW2.9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NemchenkovVG_HW2.9
+//
+//  Created by Владимир Немченков on 04.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
